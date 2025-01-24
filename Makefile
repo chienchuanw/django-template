c9db83c7276e1ff3f90166d25c3132a14f01962a
chienchuanw <chienchuanwww@gmail.com>
2025-01-24 22:11:03 +0100

    feat: add test for app 'pages'

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
 server:
 	poetry run python manage.py runserver
+
+test:
+	@if [ -z "$(filter-out $@,$(MAKECMDGOALS))" ]; then \
+		echo "Error: You must specify an app to test. Usage: make test <app_name>"; \
+		exit 1; \
+	fi
+	@poetry run python manage.py test $(filter-out $@,$(MAKECMDGOALS))
+
+%:
+	@:
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+# Test functions must start with the word "test"
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_reverse_url(self):
+        response = self.client.get(reverse("pages:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Home")
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get("/")
+        self.assertNotContains(response, "Hi there! I should not be on the page.")
+
+    def test_home_page_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "pages/home.html")
