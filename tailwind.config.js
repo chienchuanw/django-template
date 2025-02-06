33b50681658bc796ad96c636d9d437aae2073a33
chienchuanw <chienchuanwww@gmail.com>
2025-02-06 17:27:34 +0800

    style: login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./templates/**/*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        
+      ]
+    }
   },
   plugins: [],
 }
