912e451d372ad58bd977cec9081bfb5d22d73941
chienchuanw <chienchuanwww@gmail.com>
2025-02-06 19:41:48 +0800

    style: register page

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
+def register(request: HttpRequest) -> HttpResponse:
+    return render(request, "accounts/register.html")
+
+
 def login(request: HttpRequest) -> HttpResponse:
     return render(request, "accounts/login.html")
