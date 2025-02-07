f22775312c1fd4e6be15233186309a4e44feca96
chienchuanw <chienchuanwww@gmail.com>
2025-02-07 17:45:08 +0800

    feat: auto-login after registering successfully

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import FormView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
+from django.contrib.auth import login as django_login
 
 
 def register(request: HttpRequest) -> HttpResponse:
@@ -16,7 +17,13 @@ class UserRegisterView(FormView):
     success_url = reverse_lazy("home")
 
     def form_valid(self, form):
-        form.save()
+        user = form.save()
+
+        # Assign specific backend validation from AUTHENTICATION_BACKENDS in settings.py
+        user.backend = "django.contrib.auth.backends.ModelBackend"
+
+        # Auto-login after register successfully
+        django_login(self.request, user, backend=user.backend)
         return redirect(self.get_success_url())
 
 
