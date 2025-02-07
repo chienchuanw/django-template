93d0e192fb7d7928f66724bd5107332df4bd90f6
chienchuanw <chienchuanwww@gmail.com>
2025-02-07 17:32:16 +0800

    feat: implement register functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import UserRegisterView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", views.login, name="login"),
 ]
