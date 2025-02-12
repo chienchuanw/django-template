efcc421931dadf756c0a3c0017f0952955c76995
chienchuanw <chienchuanwww@gmail.com>
2025-02-12 15:02:35 +0800

    feat: allow user to read and edit profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, UserLogoutView
+from .views import (
+    UserRegisterView,
+    UserLoginView,
+    UserLogoutView,
+    UserProfileView,
+    UserProfileEditView,
+)
+from . import views
 
 app_name = "accounts"
 
@@ -7,4 +14,6 @@ urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("profile/", UserProfileView.as_view(), name="profile"),
+    path("profile/edit/", UserProfileEditView.as_view(), name="edit"),
 ]
