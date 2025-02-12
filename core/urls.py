03ca98210036c99adf0bec69d35d4d099770c770
chienchuanw <chienchuanwww@gmail.com>
2025-02-12 19:37:48 +0800

    feat: read CSV file and show on web page

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     # allauth must come after if we want to customize login page
     path("accounts/", include("allauth.urls")),
+    path("parkings/", include("parkings.urls")),
 ]
