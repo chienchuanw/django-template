2c326a6fa1c23a51a8aa6d35d2673163af11d390
chienchuanw <chienchuanwww@gmail.com>
2025-02-12 16:40:38 +0800

    modify: user can edit mobile and birthday in profile page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from accounts.models import UserProfile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -32,3 +33,31 @@ class CustomUserCreationForm(UserCreationForm):
             user.save()
 
         return user
+
+
+class UserProfileForm(forms.ModelForm):
+    username = forms.CharField(required=True, label="Username")
+    email = forms.EmailField(required=True, label="Email")
+
+    class Meta:
+        model = UserProfile
+        fields = ["mobile", "birthday"]
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields["username"].initial = user.username
+            self.fields["email"].initial = user.email
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        user = profile.user
+        user.username = self.cleaned_data["username"]
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+            profile.save()
+
+        return profile
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    # related_name allows reverse query from User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    mobile = models.CharField(max_length=20, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
