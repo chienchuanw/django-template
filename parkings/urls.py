69909b710d224186a96e5e51377481e0d16478ec
chienchuanw <chienchuanwww@gmail.com>
2025-02-14 13:01:53 +0800

    feat: use command 'import_parkinglots' to imort data successfully

--- parkings/urls.py
+++ parkings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from parkings.views import ParkingIndexView
+from parkings.views import ParkingIndexView, ParkingDetailView
 
 app_name = "parkings"
 
 urlpatterns = [
     path("", ParkingIndexView.as_view(), name="index"),
+    path("detail/", ParkingDetailView.as_view(), name="detail"),
 ]
--- parkings/views.py
+++ parkings/views.py
@@ -1,7 +1,8 @@
 import csv, os
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from django.conf import settings
+from typing import Any
 
 
 class ParkingIndexView(TemplateView):
@@ -11,33 +12,63 @@ class ParkingIndexView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         # specify CSV path
-        csv_file_path = os.path.join(
+        CSV_FILE_PATH = os.path.join(
             settings.BASE_DIR, "data/input/新北市公有路外停車場即時賸餘車位數.csv"
         )
 
         # read and load CSV
-        with open(csv_file_path, newline="", encoding="utf-8-sig") as csvfile:
-            reader = csv.reader(csvfile)
-            headers = next(reader)
-            data_list = list(row for row in reader)
+        with open(CSV_FILE_PATH, newline="", encoding="utf-8-sig") as csvfile:
+            # reader = csv.reader(csvfile)
+            reader = csv.DictReader(csvfile)
 
-        # re-assemble data from CSV
-        parking_data = [
-            {
-                "name": "test",
-                "id": int(parking_id),
-                "status": (
-                    "Error"
-                    if int(remain) == -9
-                    else "None" if int(remain) == 0 else "Available"
-                ),
-                "remain": int(remain) if int(remain) > 9 else 0,
-            }
-            for parking_id, remain in data_list
-        ]
+            # re-assemble data from CSV
+            parking_data = list(
+                {
+                    "id": int(row["ID"]),
+                    "remain": (
+                        0
+                        if int(row["AVAILABLECAR"]) == -9
+                        else int(row["AVAILABLECAR"])
+                    ),
+                    "name": "test",
+                    "status": (
+                        "Error"
+                        if int(row["AVAILABLECAR"]) == -9
+                        else "None" if int(row["AVAILABLECAR"]) == 0 else "Available"
+                    ),
+                }
+                for row in reader
+            )
 
         # send to html as context
-        context["headers"] = headers
-        context["parking_data"] = parking_data
+        context.update(
+            {
+                "parking_data": parking_data,
+            }
+        )
 
+        return context
+
+
+class ParkingDetailView(TemplateView):
+    template_name = "parkings/detail.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        CSV_FILE_PATH = os.path.join(
+            settings.BASE_DIR, "data/input/新北市路外公共停車場資訊.csv"
+        )
+
+        with open(CSV_FILE_PATH, newline="", encoding="utf-8-sig") as csvfile:
+            reader = csv.reader(csvfile)
+            header = next(reader)
+            content = list(row for row in reader)
+
+        context.update(
+            {
+                "header": header,
+                "content": content,
+            }
+        )
         return context
